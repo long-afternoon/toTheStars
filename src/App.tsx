7e5296a52dc04bd9958af3d0c8b85dff165a2a1b
long-afternoon <shiisuii900@gmail.com>
2026-03-08 00:59:11 +0530

    added project files

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import StarWrite from './Write'
+
+function App() {
+
+  return (
+    <>
+      <StarWrite/>
+    
+
+    </>
+  )
+}
+
+export default App
---
+++ src/Write.tsx
@@ -0,0 +1,25 @@
+import './Write.css'
+
+const hints: string[]= [
+    "Write whatever's on your mind...",
+    "Tell me how you're feeling right now",
+    "Write a letter to the stars tonight",
+    "Tell the stars what today felt like",
+    "Leave a message for the night",
+    "Before sleep, write a little"
+];
+
+
+function StarWrite() {
+    return (
+        <div className="writeContainer">
+            <textarea id="thoughtBox"
+                placeholder={hints[Math.floor(Math.random() * hints.length)]} name="thoughtsBox">
+            </textarea>
+            <button id='sendThought'>SEND</button>
+        </div>
+    )
+}
+
+
+export default StarWrite
